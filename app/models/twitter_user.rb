4d6223d6bab4850f8f7d44a61a3fe4af84a8193f
Nemrow <jnemrow@shopittome.com>
2014-09-04 16:40:08 -0700

    Refactor to edit image
    Also added some temporary code to use fixtures instead of hitting the api each time. Twitter rate limits like CRAZY

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -12,8 +12,7 @@ class TwitterUser
   def mutual_followers(twitter_handle_for_intersection)
     users_follower_ids = get_follower_ids(@twitter_handle)
     follower_ids_to_intersect = get_follower_ids(twitter_handle_for_intersection)
-    mutual_followers = users_follower_ids.to_a & follower_ids_to_intersect.to_a
-    client.users(mutual_followers)
+    get_mutual_followers_data(users_follower_ids, follower_ids_to_intersect)
   end
 
   private
@@ -24,11 +23,34 @@ class TwitterUser
       end
     end
 
+    def get_mutual_followers_data(users_follower_ids, follower_ids_to_intersect)
+      if Rails.env == "development"
+        return JSON.parse(File.read('spec/fixtures/mutual_followers.json'))["mutual_followers"]
+      end
+      mutual_followers_ids = users_follower_ids.to_a & follower_ids_to_intersect.to_a
+      mutual_follower_objects = client.users(mutual_followers_ids)
+      edit_twitter_users_data(mutual_follower_objects)
+    end
+
+    def edit_twitter_users_data(mutual_follower_objects)
+      mutual_follower_objects.map do |current_follower|
+        current_follower.attrs.merge(:profile_image_url => edit_image_url(current_follower.profile_image_url))
+      end
+    end
+
+    def edit_image_url(image_url)
+      image_url.to_s.gsub("_normal", "")
+    end
+
     def get_tweets(twitter_handle, quantity)
       client.user_timeline(twitter_handle, {:count => quantity})
     end
 
     def get_follower_ids(twitter_handle)
+      if Rails.env == "development"
+        return JSON.parse(File.read('spec/fixtures/follower_ids_1.json'))["follower_ids"] if (twitter_handle == "nemrow")
+        return JSON.parse(File.read('spec/fixtures/follower_ids_2.json'))["follower_ids"] if (twitter_handle == "dpolaske")
+      end
       client.follower_ids(twitter_handle)
     end
 end
