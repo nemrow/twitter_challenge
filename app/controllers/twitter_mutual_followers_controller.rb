a0f75500e1ce91c2546c15f979ef0f6d4e873fee
Nemrow <jnemrow@shopittome.com>
2014-09-05 09:39:16 -0700

    split into 2 lines for readability

--- app/controllers/twitter_mutual_followers_controller.rb
+++ app/controllers/twitter_mutual_followers_controller.rb
@@ -1,5 +1,6 @@
 class TwitterMutualFollowersController < ApplicationController
   def show
-    render :json => TwitterDataPresenter.new(params[:twitter_handle_main]).mutual_followers_as_json(params[:twitter_handle_for_intersection])
+    presenter = TwitterDataPresenter.new(params[:twitter_handle_main])
+    render :json => presenter.mutual_followers_as_json(params[:twitter_handle_for_intersection])
   end
 end
