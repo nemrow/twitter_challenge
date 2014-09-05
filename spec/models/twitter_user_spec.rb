2f2ab6ee76958a5d2c348a82e51fc68a9664f3a0
Nemrow <nemrowj@gmail.com>
2014-09-05 13:53:34 -0700

    Change stub method name to reflect to class method

--- spec/models/twitter_user_spec.rb
+++ spec/models/twitter_user_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe TwitterUser do
 
   it "should return a list of the users mututal followers" do
-    TwitterUser.any_instance.stub(:get_follower_ids).with("nemrow").and_return(json_fixture('follower_ids_1.json'))
-    TwitterUser.any_instance.stub(:get_follower_ids).with("dpolaske").and_return(json_fixture('follower_ids_2.json'))
+    TwitterUser.any_instance.stub(:get_following_ids).with("nemrow").and_return(json_fixture('follower_ids_1.json'))
+    TwitterUser.any_instance.stub(:get_following_ids).with("dpolaske").and_return(json_fixture('follower_ids_2.json'))
     TwitterUser.any_instance.stub(:get_users_by_twitter_ids).with([294784306,73719806,117567114,2337397724]).and_return(json_fixture('mutual_followers.json'))
     assert_equal 4, TwitterUser.new("nemrow").mutual_followers("dpolaske").count
   end
