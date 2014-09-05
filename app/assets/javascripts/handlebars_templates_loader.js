d83ff32c1ed19d19592c80ca29b673fffe8c75f4
Nemrow <jnemrow@shopittome.com>
2014-09-05 09:38:43 -0700

    Port method to jquery

--- app/assets/javascripts/handlebars_templates_loader.js
+++ app/assets/javascripts/handlebars_templates_loader.js
@@ -1,9 +1,9 @@
 TEMPLATES = {};
 $(document).ready(function () {
-  var scriptTags = document.querySelectorAll('script[type="text/x-handlebars-template"]');
-  Array.prototype.forEach.call(scriptTags, function(script) {
-    var name = script.id;
-    var content = script.innerHTML;
+  var scriptTags = $('script[type="text/x-handlebars-template"]');
+  $.each(scriptTags, function () {
+    var name = this.id;
+    var content = this.innerHTML;
     TEMPLATES[name] = Handlebars.compile(content);
   });
 });
