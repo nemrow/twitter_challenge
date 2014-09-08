c3f1d6121a47ebb9c585621b0d3dfd70b1039fd6
swyman <sam.wyman@gmail.com>
2014-09-08 14:42:55 -0700

    Update twitter_mutual_followers_controller.rb

--- app/controllers/twitter_mutual_followers_controller.rb
+++ app/controllers/twitter_mutual_followers_controller.rb
@@ -1,6 +1,6 @@
 class TwitterMutualFollowersController < ApplicationController
   def show
-    mutual_followers = TwitterUser.new(params[:twitter_handle_main]).mutual_followers(params[:twitter_handle_for_intersection])
+    mutual_followers = TwitterUser.new(params[:twitter_handle_main]).mutual_followers(TwitterUser.new(params[:twitter_handle_for_intersection]))
     render :json => mutual_followers
   end
 end
